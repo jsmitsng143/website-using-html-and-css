6647f293372a1a0348898debb74f9e00b3a1cd2e
DesmondANIMUS <desmondmiles36@gmail.com>
2017-04-25 20:45:39 +0530

    many smol changes

--- CV.go
+++ CV.go
@@ -21,6 +21,8 @@ type cvData struct {
 	Web       string `bson:"web"`
 	Addr      string `bson:"addr"`
 	Gender    string `bson:"gender"`
+	DoB       string `bson:"dob"`
+	Country   string `bson:"country"`
 
 	// Education below
 	XPer  string `bson:"xper"`
@@ -122,6 +124,61 @@ func newSub(w http.ResponseWriter, r *http.Request) {
 
 	if r.Method == http.MethodPost {
 		data.ImageName = uploadPage(w, r)
+		data.Name = r.FormValue("flname")
+		data.Email = r.FormValue("umail")
+		data.Phone = r.FormValue("ucontact")
+		data.Web = r.FormValue("web")
+		data.DoB = r.FormValue("dob")
+		data.Addr = r.FormValue("addr")
+		data.Gender = r.FormValue("gender")
+		data.Country = r.FormValue("con")
+
+		data.XPer = r.FormValue("xper")
+		data.Xname = r.FormValue("xname")
+		data.Xyear = r.FormValue("xyear")
+
+		data.XIIper = r.FormValue("xiiper")
+		data.XIIname = r.FormValue("xiiname")
+		data.XIIyear = r.FormValue("xiiyear")
+
+		data.Objective = r.FormValue("uobj")
+
+		data.ComName1 = r.FormValue("comname1")
+		data.ComName2 = r.FormValue("comname2")
+		data.ComName3 = r.FormValue("comname3")
+		data.ComName4 = r.FormValue("comname4")
+		data.ComName5 = r.FormValue("comname5")
+		data.ComName6 = r.FormValue("comname6")
+		data.ComName7 = r.FormValue("comname7")
+		data.ComName8 = r.FormValue("comname8")
+
+		data.JobTitle1 = r.FormValue("jobtitle1")
+		data.JobTitle2 = r.FormValue("jobtitle2")
+		data.JobTitle3 = r.FormValue("jobtitle3")
+		data.JobTitle4 = r.FormValue("jobtitle4")
+		data.JobTitle5 = r.FormValue("jobtitle5")
+		data.JobTitle6 = r.FormValue("jobtitle6")
+		data.JobTitle7 = r.FormValue("jobtitle7")
+		data.JobTitle8 = r.FormValue("jobtitle8")
+
+		data.JobDates1 = r.FormValue("jobdate1")
+		data.JobDates2 = r.FormValue("jobdate2")
+		data.JobDates3 = r.FormValue("jobdate3")
+		data.JobDates4 = r.FormValue("jobdate4")
+		data.JobDates5 = r.FormValue("jobdate5")
+		data.JobDates6 = r.FormValue("jobdate6")
+		data.JobDates7 = r.FormValue("jobdate7")
+		data.JobDates8 = r.FormValue("jobdate8")
+
+		data.Skill1 = r.FormValue("skill1")
+		data.Skill2 = r.FormValue("skill2")
+		data.Skill3 = r.FormValue("skill3")
+		data.Skill4 = r.FormValue("skill4")
+		data.Skill5 = r.FormValue("skill5")
+		data.Skill6 = r.FormValue("skill6")
+		data.Skill7 = r.FormValue("skill7")
+		data.Skill8 = r.FormValue("skill8")
+		data.Skill9 = r.FormValue("skill9")
 
 		err := dataInDb(data)
 		if err != nil {
@@ -152,7 +209,7 @@ func uploadPage(w http.ResponseWriter, r *http.Request) string {
 
 		nameUpdate, _ := uuid.NewV4()
 		hdr.Filename = nameUpdate.String()
-		filePath := "./images/" + hdr.Filename + ".png"
+		filePath := "./assets/images/" + hdr.Filename + ".png"
 
 		//writing file by creating one
 		dst, err := os.Create(filePath)
